c869060aa4944baa6f0b56843a8134a3d6a2250d
bayrambinbir <38406467+bayrambinbir@users.noreply.github.com>
2023-10-25 15:57:49 -0500

    backend for paypal

--- backend/controller/orderController.js
+++ backend/controller/orderController.js
@@ -64,14 +64,28 @@ const getMyOrderById = asyncHandler(async (req, res) => {
 });
 
 // @desc    Update order to paid
-// @route   GET /api/orders/:id/pay
+// @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("update order to paid");
+  const order = await Order.findById(res, params.id);
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    order.paymentResults = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+    };
+    const updatedOrder = await order.save();
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
 });
 
 // @desc    Update order to delivered
-// @route   GET /api/orders/:id/deliver
+// @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
 const updateOrderToDelivered = asyncHandler(async (req, res) => {
   res.send("update order to delivered");
